6a9463dcd0898dca5cd5240f5d3e4e5f25a6824e
joicenunes <anunes.joice@gmail.com>
2024-03-18 19:39:30 -0300

    feat: reproducing code with error

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,42 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <div class="example">
+      <v-text-field
+        v-model="valor"
+        dense
+        outlined
+        placeholder="0,00"
+        prefix="R$"
+        @input="tratarValor"
+      ></v-text-field>
+    </div>
   </div>
 </template>
 
-<script>
-import HelloWorld from "./components/HelloWorld.vue";
-
+<script lang="ts">
 export default {
   name: "App",
-  components: {
-    HelloWorld,
+  data: () => ({
+    valor: "",
+  }),
+  methods: {
+    tratarValor: async function () {
+      this.valor = this.valor.replace(/\D/g, "");
+      await this.$nextTick();
+      if (this.valor.length > 0) {
+        const valorEmDecimais = Number(this.valor) / 100;
+        this.valor = valorEmDecimais.toString();
+      }
+    },
   },
 };
 </script>
 
 <style>
+.example {
+  max-width: 200px;
+  margin: 100px auto;
+}
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
