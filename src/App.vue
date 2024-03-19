18299469f76e6050328f15466eadee9a7615c5fd
joicenunes <anunes.joice@gmail.com>
2024-03-18 21:00:41 -0300

    feat: adiciondo tratativas
    tratarValorPressionado: não permitir dados não numéricos no campo
    tratarValor: formatação correta de valores com 0s no meio; troca de . por ,
    
    todo? usar internacionalização de valor monetário com formato BRL?

--- src/App.vue
+++ src/App.vue
@@ -7,6 +7,7 @@
         outlined
         placeholder="0,00"
         prefix="R$"
+        @keypress="tratarValorPressionado"
         @input="tratarValor"
       ></v-text-field>
     </div>
@@ -20,12 +21,16 @@ export default {
     valor: "",
   }),
   methods: {
+    tratarValorPressionado: function (event) {
+      let char = String.fromCharCode(event.keyCode);
+      if (/\d/.test(char)) return true;
+      else event.preventDefault();
+    },
     tratarValor: async function () {
       this.valor = this.valor.replace(/\D/g, "");
-      await this.$nextTick();
       if (this.valor.length > 0) {
         const valorEmDecimais = Number(this.valor) / 100;
-        this.valor = valorEmDecimais.toString();
+        this.valor = valorEmDecimais.toFixed(2).replace(".", ",");
       }
     },
   },
